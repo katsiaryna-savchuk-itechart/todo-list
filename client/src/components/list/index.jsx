4601ad4e2159b8d4f516694b04e8420eb64e8fa5
katsiaryna-savchuk-itechart <Katsiaryna.Savchuk@itechart-group.com>
2022-05-03 15:31:32 +0300

    fix map

--- client/src/components/list/index.jsx
+++ client/src/components/list/index.jsx
@@ -11,7 +11,7 @@ import {
 export const List = ({ list, onDeleteItem, onClear }) => (
   <Container>
     <Wrapper>
-      {list.map(({ action, date, id }) => (
+      {list.length && list.map(({ action, date, id }) => (
         <Item key={id}>
           <TextWrapper>
             <ActionName>{action}</ActionName>
